2e64c513c95b57f39a464291ff057059adadd21d
Ekaterina Rykova <katrykova2002@yandex.ru>
2022-11-13 11:22:46 +0300

    booking-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { AuthPage } from './pages/auth-pages';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import { BookingPage } from './pages/booking/booking-page';
 
 function App() {
   return (
     <div className="App">
-      <AuthPage />
+      <BrowserRouter>
+      <div>
+          <Link to={'/auth'}>Auth</Link> 
+          <Link to={'/booking'}>Booking</Link> 
+        </div>
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/booking" element={<BookingPage />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
